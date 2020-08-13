b304ffa42ca240842774369b09907c8e5b604be9
LESAK13 <lesak13@gmail.com>
2020-08-12 20:52:15 -0400

    Updates

---
+++ precache-manifest.8eb265a40a78a5f0ae7ee091597b2bcc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2886ee62862c6fa259531c74d43a41ac",
+    "url": "/Homework-20/index.html"
+  },
+  {
+    "revision": "786078116f572fa02119",
+    "url": "/Homework-20/static/css/main.9123e583.chunk.css"
+  },
+  {
+    "revision": "c3daaa38352be5135101",
+    "url": "/Homework-20/static/js/2.fcb39e99.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Homework-20/static/js/2.fcb39e99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "786078116f572fa02119",
+    "url": "/Homework-20/static/js/main.cdf56ae4.chunk.js"
+  },
+  {
+    "revision": "61c35929ce499cdc4df7",
+    "url": "/Homework-20/static/js/runtime-main.9f2d1ffe.js"
+  }
+]);
